7f142cff40ba43b84ca705300325a40fc02d0328
abhishek kamat <abhishekkamat142@gmail.com>
2025-12-02 17:52:59 +0530

    Added rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser"
+import ratelimit from "express-rate-limit";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +11,14 @@ import { ENV } from "./lib/env.js";
 const app = express();
 
 const PORT = ENV.PORT || 3000;
+
+const limiter = ratelimit({
+    windowMs: 1*60*1000,
+    max: 100,
+    statusCode: 429,
+    message: "Too many request. Please try again later",
+});
+app.use(limiter);
 
 app.use(express.json())
 app.use(cookieParser());
